c2b31413c37444b3900f02a9a32e0027ed838a0a
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-11-09 15:07:13 +0100

    product route

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,8 +1,16 @@
-import express from "express"
-const pRouter = express.Router()
+import express from "express";
+import authmiddleware from "../Middleware/isUser.js";
+import verifyAdmin from "../Middleware/isAdmin.js";
+import {
+  httpGetAllProduct,
+  httpCreateProduct,
+  httpUpdateProduct,
+  httpDeleteProduct,
+} from "../controllers/productController.js";
+const pRouter = express.Router();
 
-pRouter.post("/create",)
-pRouter.get("/get",)
-pRouter.put("/update");
-pRouter.delete("/delete");
-export default pRouter
+pRouter.post("/create", authmiddleware, httpCreateProduct);
+pRouter.get("/get", authmiddleware,verifyAdmin, httpGetAllProduct);
+pRouter.put("/update", authmiddleware,verifyAdmin, httpUpdateProduct);
+pRouter.delete("/delete", authmiddleware, verifyAdmin,httpDeleteProduct);
+export default pRouter;
