eb39160367f58543fbcfee460e0dbe800d142096
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-11-13 13:34:51 +0100

    minor changes

---
+++ src/controllers/userProductController.js
@@ -0,0 +1,7 @@
+export const httpAddUserAddress = (req, res) => {};
+export const addToProductCart = (req, res) => {};
+export const addToProdcutToWishlist = (req, res) => {};
+export const getUserWishlist = (req, res) => {};
+export const getUserCart = (req, res) => {};
+export const removeProdcutfromWishlist = (req, res) => {};
+export const removeProdcutfromCart = (req, res) => {};
